87371d340e8b838e42dd85c949c9fd608ac3ee48
Mykola-Shevchuk <mykola.shevchuk.ua@gmail.com>
2016-11-13 22:11:43 +0200

    Modified webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         test: /\.css$/,
         loader: "style-loader!css-loader",
         exclude: [/node_modules/, /public/]
-      }
+      },
       {
         test: /\.jsx$/,
         loader: "babel",
